fdcd99922c0fd449816277a13b3e5b4c4a99a7dd
kray19 <karenraymond@brandeis.edu>
2017-11-19 05:28:57 -0500

    Added insert statements.

--- src/main/resources/META-INF/sql/test-data.sql
+++ src/main/resources/META-INF/sql/test-data.sql
@@ -13,3 +13,6 @@ INSERT INTO inventoryitem
 `SupplierId`)
 VALUES
 ('Battery', '123456780011', '12345', 'Apple', 10, 'Arizona', '5', 'Single', ' 11/17/2017', 1000.00, 1);
+
+insert into PURCHASE_ORDER_HEADER (order_total_price) values (1000.00);
+insert into PURCHASE_ORDERS (qty_ordered, delivery_date, unit_price) values ('8', 11/30/2017, 100.00);
