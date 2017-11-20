e38b9638f3096ac01bac7f1e2ec49d90a6779a44
kray19 <karenraymond@brandeis.edu>
2017-11-20 11:16:28 -0500

    Added service layer.

---
+++ src/main/java/edu/brandeis/spring/mvc/service/InventoryItemServiceImpl.java
@@ -0,0 +1,47 @@
+package edu.brandeis.spring.mvc.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.google.common.collect.Lists;
+
+import edu.brandeis.spring.mvc.repository.InventoryItemRepository;
+import edu.brandeis.spring.mvc.domain.InventoryItem;
+
+@Repository
+@Transactional
+@Service("inventoryItemService")
+public class InventoryItemServiceImpl implements InventoryItemService {
+    private InventoryItemRepository inventoryItemRepository;
+
+    @Override
+    @Transactional(readOnly=true)
+    public List<InventoryItem> findAll() {
+        return Lists.newArrayList(inventoryItemRepository.findAll());
+    }
+
+    @Override
+    @Transactional(readOnly=true)
+    public InventoryItem findById(Long id) {
+        return inventoryItemRepository.findOne(id);
+    }
+
+    @Override
+    public InventoryItem save(InventoryItem inventoryItem) {
+        return inventoryItemRepository.save(inventoryItem);
+    }
+
+    @Autowired
+    public void setInventoryItemRepository(InventoryItemRepository inventoryItemRepository) {
+        this.inventoryItemRepository = inventoryItemRepository;
+    }
+
+   
+
+}
---
+++ src/main/java/edu/brandeis/spring/mvc/service/SupplierServiceImpl.java
@@ -0,0 +1,45 @@
+package edu.brandeis.spring.mvc.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.google.common.collect.Lists;
+
+import edu.brandeis.spring.mvc.repository.SupplierRepository;
+import edu.brandeis.spring.mvc.domain.Supplier;
+
+@Repository
+@Transactional
+@Service("supplierService")
+public class SupplierServiceImpl implements SupplierService {
+    private SupplierRepository supplierRepository;
+
+    @Override
+    @Transactional(readOnly=true)
+    public List<Supplier> findAll() {
+        return Lists.newArrayList(supplierRepository.findAll());
+    }
+
+    @Override
+    @Transactional(readOnly=true)
+    public Supplier findById(Long id) {
+        return supplierRepository.findOne(id);
+    }
+
+    @Override
+    public Supplier save(Supplier supplier) {
+        return supplierRepository.save(supplier);
+    }
+
+    @Autowired
+    public void setSupplierRepository(SupplierRepository supplierRepository) {
+        this.supplierRepository = supplierRepository;
+    }
+
+}
