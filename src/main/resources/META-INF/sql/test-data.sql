fb0d4e36ba6744c030103013a5e8f79f16f31376
J Cheyo Jimenez <cheyo@masters3d.com>
2017-12-03 17:20:21 -0800

    converted all the test data to be generated from workbench

--- src/main/resources/META-INF/sql/test-data.sql
+++ src/main/resources/META-INF/sql/test-data.sql
@@ -1,30 +1,6 @@
-insert into supplier
-(name,
-streetname,
-city,
-state,
-zipcode,
-phone,
-edi,
-payment,
-incoterms)
-values
-('Littleton Lumber', '55 White Street', 'Littleton', 'MA', '01460', '978-486-9877', 'paper', 'net 30 days', 'none');
 
-INSERT INTO inventoryitem 
-(`ItemName`,
-`ItemUPCNumber`,
-`ItemSKU`,
-`ManufacturerName`,
-`InventoryOnHand`,
-`WarehouseLocation`,
-`ReorderQuantity`,
-`ReorderUnit`,
-`BackorderDate`,
-`PerItemRetailSalePrice`,
-`SupplierId`)
-VALUES
-('Battery', '123456780011', '12345', 'Apple', 10, 'Arizona', '5', 'Single', ' 11/17/2017', 1000.00, 1);
 
-insert into PURCHASE_ORDER_HEADER (order_total_price) values (1000.00);
-insert into PURCHASE_ORDERS (qty_ordered, delivery_date, unit_price) values ('8', 11/30/2017, 100.00);
+INSERT INTO `SUPPLIER` (`ID`,`NAME`,`STREETNAME`,`CITY`,`STATE`,`ZIPCODE`,`PHONE`,`EDI`,`PAYMENT`,`INCOTERMS`) VALUES (1,'Littleton Lumber','55 White Street','Littleton','MA','1460','978-486-9877','paper','net 30 days','none');
+INSERT INTO `InventoryItem` (`ItemId`,`ItemName`,`ItemUPCNumber`,`ItemSKU`,`ManufacturerName`,`InventoryOnHand`,`WarehouseLocation`,`ReorderQuantity`,`ReorderUnit`,`BackorderDate`,`PerItemRetailSalePrice`,`SupplierId`) VALUES (1,'Battery',' 123456780011',' 12345',' Apple',10,' Arizona', 5,' Single',' 11/17/2017',1000,1);
+INSERT INTO `PURCHASE_ORDER_HEADER` (`ID`,`SUPPLIER_ID`,`ORDER_TOTAL_PRICE`) VALUES (1,1,1000.00);
+INSERT INTO `PURCHASE_ORDERS` (`ID`,`PURCHASE_ORDER_ID`,`ITEM_ID`,`QTY_ORDERED`,`DELIVERY_DATE`,`UNIT_PRICE`) VALUES (NULL,1,1,8,'11/30/2017',100.00);
