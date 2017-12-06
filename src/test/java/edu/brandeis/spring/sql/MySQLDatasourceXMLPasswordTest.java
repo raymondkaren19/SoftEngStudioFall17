0036d3241c7a60f5d7b90cca4f2dc44a6b26c6e4
J Cheyo Jimenez <cheyo@masters3d.com>
2017-12-06 02:35:43 -0800

    clean up commnets

--- src/test/java/edu/brandeis/spring/sql/MySQLDatasourceXMLPasswordTest.java
+++ src/test/java/edu/brandeis/spring/sql/MySQLDatasourceXMLPasswordTest.java
@@ -1,15 +1,4 @@
 package edu.brandeis.spring.sql;
-
-import junit.framework.TestCase;
-import java.io.File;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-// The Production and Staging server are expecting sql username and password 
 // to be a the same so a script can swap their values. 
 // This makes sure that this does not change with out updating the other scripts.
 public class MySQLDatasourceXMLPasswordTest extends MySQLDatasourceXMLUsernameTest {
