d8e69953743b7e2d50ff65ea28018806acb8d460
Lakshmi <lmann0001@brandeis.edu>
2017-12-16 19:00:15 -0500

    Generate purchae orders by request

--- src/main/resources/META-INF/sql/test-data.sql
+++ src/main/resources/META-INF/sql/test-data.sql
@@ -13,8 +13,14 @@ COMMIT;
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `InventoryDB`;
-INSERT INTO `InventoryDB`.`InventoryItem` (`ItemId`, `ItemName`, `ItemUPCNumber`, `ItemSKU`, `ManufacturerName`, `InventoryOnHand`, `WarehouseLocation`, `ReorderQuantity`, `ReorderUnit`, `BackorderDate`, `PerItemRetailSalePrice`, `SupplierId`) VALUES (1, 'Battery', '123456780011', '12345', 'Apple', 10, 'Arizona',  5, 'Single', '2017-11-17', 1000, 1);
+INSERT INTO `InventoryDB`.`InventoryItem` (`ItemId`, `ItemName`, `ItemUPCNumber`, `ItemSKU`, `ManufacturerName`, `InventoryOnHand`, `WarehouseLocation`, `ReorderQuantity`, `ReorderUnit`, `BackorderDate`, `PerItemRetailSalePrice`, `SupplierId`) VALUES (1, 'Battery',  '123456780011', '12345', 'Apple', 10, 'Arizona',  5, 'Single', '2017-11-17', 1000, 1);
 INSERT INTO `InventoryDB`.`InventoryItem` (`ItemId`, `ItemName`, `ItemUPCNumber`, `ItemSKU`, `ManufacturerName`, `InventoryOnHand`, `WarehouseLocation`, `ReorderQuantity`, `ReorderUnit`, `BackorderDate`, `PerItemRetailSalePrice`, `SupplierId`) VALUES (2, 'Keyboard', '123456780012', '127895', 'Google', 10, 'CA', '5', 'Single', ' 12/04/2017', 100.00, 1);
+INSERT INTO `InventoryDB`.`InventoryItem` (`ItemId`, `ItemName`, `ItemUPCNumber`, `ItemSKU`, `ManufacturerName`, `InventoryOnHand`, `WarehouseLocation`, `ReorderQuantity`, `ReorderUnit`, `BackorderDate`, `PerItemRetailSalePrice`, `SupplierId`) VALUES (3, 'Soundbar', '123985678002', '120895', 'Bose', 1, 'MA', '3', 'Single', ' 1/04/2018', 600.00, 1);
+INSERT INTO `InventoryDB`.`InventoryItem` (`ItemId`, `ItemName`, `ItemUPCNumber`, `ItemSKU`, `ManufacturerName`, `InventoryOnHand`, `WarehouseLocation`, `ReorderQuantity`, `ReorderUnit`, `BackorderDate`, `PerItemRetailSalePrice`, `SupplierId`) VALUES (4, 'Tablet',    '123256780012', '127825', 'Microsoft', 5, 'AN', '6', 'Single', ' 12/04/2017', 300.00, 1);
+INSERT INTO `InventoryDB`.`InventoryItem` (`ItemId`, `ItemName`, `ItemUPCNumber`, `ItemSKU`, `ManufacturerName`, `InventoryOnHand`, `WarehouseLocation`, `ReorderQuantity`, `ReorderUnit`, `BackorderDate`, `PerItemRetailSalePrice`, `SupplierId`) VALUES (5, 'Headphone', '123498780012', '187895', 'Beats', 4, 'NC', '5', 'Single', ' 12/04/2017', 210.00, 2);
+INSERT INTO `InventoryDB`.`InventoryItem` (`ItemId`, `ItemName`, `ItemUPCNumber`, `ItemSKU`, `ManufacturerName`, `InventoryOnHand`, `WarehouseLocation`, `ReorderQuantity`, `ReorderUnit`, `BackorderDate`, `PerItemRetailSalePrice`, `SupplierId`) VALUES (6, 'Echo',       '120956780012', '167895', 'Amazon', 3, 'GA', '5', 'Single', ' 12/04/2017', 89.00, 2);
+INSERT INTO `InventoryDB`.`InventoryItem` (`ItemId`, `ItemName`, `ItemUPCNumber`, `ItemSKU`, `ManufacturerName`, `InventoryOnHand`, `WarehouseLocation`, `ReorderQuantity`, `ReorderUnit`, `BackorderDate`, `PerItemRetailSalePrice`, `SupplierId`) VALUES (7, 'Chromecast', '121856780012', '127395', 'Google', 8, 'CA', '5', 'Single', ' 12/04/2017', 30.00, 2);
+
 
 COMMIT;
 
